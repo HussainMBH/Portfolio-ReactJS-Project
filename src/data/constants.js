1c7109a806ee6cd65e4295818a1e85f24c36f3ea
Bahir Hussain <mbahirh624@gmail.com>
2024-10-28 20:15:24 +0530

    Update

--- src/data/constants.js
+++ src/data/constants.js
@@ -249,7 +249,7 @@ export const experiences = [
     company: "Colombo Mail",
     //date: "Nov 2022 - Mar 2024",
     desc: [
-      "Bill Payment and Data Management system, streamlining financial processes and ensuring accurate record-keeping.",
+      "Developed API for Bill Payment and Data Management system, streamlining financial processes and ensuring accurate record-keeping.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
